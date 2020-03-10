ea5f8a03eed40ae56a71cdc6f7c75704c41b8e60
Anne <aleurs20@gmail.com>
2020-03-10 13:38:28 +0100

    Updates voor Android Studio

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+// import firebase from 'firebase';
 import { name as appName } from './app.json';
+
+// firebase.initializeApp({
+//   databaseURL: 'https://cameraapp-62dfd.firebaseio.com',
+//   projectId: 'cameraapp-62dfd',
+// });
 
 AppRegistry.registerComponent(appName, () => App);
